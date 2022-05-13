ec25adc6eaba1124a1783562381ea1a77c013523
remone2 <jamodinm@yahoo.com>
2022-05-13 14:12:19 -0500

    Project BLL updated

--- BugTracker/BugTracker/Data/BLL/ProjectBusinessLogic.cs
+++ BugTracker/BugTracker/Data/BLL/ProjectBusinessLogic.cs
@@ -1,25 +1,52 @@
 ﻿using BugTracker.Data.DAL;
 using BugTracker.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 namespace BugTracker.Data.BLL
 {
     public class ProjectBusinessLogic
     {
-        private IRepository<Project> Repo;
+        private IRepository<Project> ProjectRepo;
+        private IRepository<Ticket> TicketRepo;
+        private ApplicationDbContext Db;
         private UserManager<ApplicationUser> UserManager;
         private RoleManager<IdentityRole> RoleManager;
 
-        public ProjectBusinessLogic(IRepository<Project> repoArg, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public ProjectBusinessLogic(ApplicationDbContext db, IRepository<Project> projRepo, IRepository<Ticekt> ticketRepo, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            Repo = repoArg;
+            Db = db;
+            ProjectRepo = projRepo;
+            TicketRepo = ticketRepo;
             UserManager = userManager;
             RoleManager = roleManager;
         }
 
         public List<Project> GetAllProjects()
         {
-            return Repo.GetAll().ToList();
+            return ProjectRepo.GetAll().ToList();
+        }
+
+        [Authorize(Roles = "Admin, Project Manager")]
+        public async void AddDeveloperToProject(string devId, int projId)
+        {
+            ApplicationUser user = await UserManager.FindByIdAsync(devId);
+            Project project = ProjectRepo.Get(projId);
+            project.Developers.Add(user);
+            user.ProjectAssigned = project;
+            ProjectRepo.Save();
+            Db.SaveChanges();
+        }
+
+        public void AddTicketToProject(int ticketId, int projId)
+        {
+            Ticket ticket = TicketRepo.Get(ticketId);
+            Project project = ProjectRepo.Get(projId);
+
+            project.Tickets.Add(ticket);
+            ticket.Project = project;
+            ProjectRepo.Save();
+            TicketRepo.Save();
         }
     }
 }
