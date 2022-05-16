bbf14bf84dca70aa51eac6088661bdb504b6b3da
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-05-16 16:55:16 -0500

    add TicketLogItem class and map its one to one relationship with TicketHistory.

---
+++ BugTracker/BugTracker/Data/Migrations/20220516214251_AddTicketLogItemClass.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTracker.Data.Migrations
+{
+    public partial class AddTicketLogItemClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NewValue",
+                table: "TicketHistory");
+
+            migrationBuilder.DropColumn(
+                name: "OldValue",
+                table: "TicketHistory");
+
+            migrationBuilder.DropColumn(
+                name: "Property",
+                table: "TicketHistory");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketLogItemId",
+                table: "TicketHistory",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TicketLogItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OldTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OldDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OldType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OldPriority = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OldStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TicketHistoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketLogItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TicketLogItem_TicketHistory_TicketHistoryId",
+                        column: x => x.TicketHistoryId,
+                        principalTable: "TicketHistory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketLogItem_TicketHistoryId",
+                table: "TicketLogItem",
+                column: "TicketHistoryId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketLogItem");
+
+            migrationBuilder.DropColumn(
+                name: "TicketLogItemId",
+                table: "TicketHistory");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewValue",
+                table: "TicketHistory",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OldValue",
+                table: "TicketHistory",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Property",
+                table: "TicketHistory",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ BugTracker/BugTracker/Models/TicketLogItem.cs
@@ -0,0 +1,15 @@
+﻿namespace BugTracker.Models
+{
+    public class TicketLogItem //One to One between (PrincipalTable)TicketHistory and TicketLogItem
+    {
+        public int Id { get; set; }
+        public string OldTitle { get; set; }
+        public string OldDescription { get; set; }
+        public string OldType { get; set; }
+        public string OldPriority { get; set; }
+        public string OldStatus { get; set; } //can only be changed by the ProjectManager of the Ticket's Project
+        public TicketHistory TicketHistory { get; set; }
+        public int TicketHistoryId { get; set; }
+    }
+}
+//image clone of the old ticket
