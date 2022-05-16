04569579ac1e278edd2eedaab6699b28bf778f43
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-05-16 15:59:17 -0500

    add Type, Priority, and Status properties to Ticket class and update the SeedData.

---
+++ BugTracker/BugTracker/Data/Migrations/20220516204658_AddPropertyPriorityTypeAndStatusToTicket.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTracker.Data.Migrations
+{
+    public partial class AddPropertyPriorityTypeAndStatusToTicket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Ticket");
+        }
+    }
+}
