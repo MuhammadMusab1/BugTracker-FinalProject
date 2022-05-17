efbdc3122db6bcaed5da5b9372909d9d51987eec
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-05-17 13:38:27 -0500

    add post method for CreateTicket() and CreateTicket.cshtml

--- BugTracker/BugTracker/Controllers/TicketController.cs
+++ BugTracker/BugTracker/Controllers/TicketController.cs
@@ -2,6 +2,7 @@
 using BugTracker.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.Controllers
 {
@@ -21,9 +22,35 @@ namespace BugTracker.Controllers
         {
             return View();
         }
+        //https://localhost:7045/ticket/createTicket
         [HttpGet]
         public IActionResult CreateTicket()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTicket([Bind("Title, Description, CreatedDate, UpdatedDate, Priority, Status, Type, SubmitterId, ProjectId")] Ticket newTicket)
+        {
+            //clear validation for properties you need but will not get from the form
+            ModelState.ClearValidationState("Project");
+            ModelState.ClearValidationState("Submitter");
+            Project projectForTicket = _db.Project.Include(project => project.ProjectManager).First(project => project.Id == newTicket.ProjectId);
+            ApplicationUser submitter = _db.Users.Include(user => user.ProjectsOwned).First(user => user.Id == newTicket.SubmitterId);
+
+            //add them manually
+            newTicket.Project = projectForTicket;
+            newTicket.Submitter = submitter;
+
+            //TryValidateModel(newTicket) will only be true once targets on ModelState.ClearValidationState() are either valid or unvalidated.
+            //if even one property is invalid the whole model is invalid.
+            if (TryValidateModel(newTicket))
+            {
+                submitter.SubmittedTickets.Add(newTicket);
+                projectForTicket.Tickets.Add(newTicket);
+                _db.Ticket.Add(newTicket);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
