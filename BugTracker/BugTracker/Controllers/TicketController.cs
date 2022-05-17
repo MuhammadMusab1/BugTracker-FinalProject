c10722534b7307fc5ba26af7f4e21d1f60b5c440
remone2 <jamodinm@yahoo.com>
2022-05-17 11:54:21 -0500

    add ticket controller

---
+++ BugTracker/BugTracker/Controllers/TicketController.cs
@@ -0,0 +1,25 @@
+﻿using BugTracker.Data;
+using BugTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class TicketController : Controller
+    {
+        private ApplicationDbContext _db { get; set; }
+        private UserManager<ApplicationUser> _userManager { get; set; }
+        private RoleManager<IdentityRole> _roleManager { get; set; }
+        public TicketController(ApplicationDbContext Db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = Db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
