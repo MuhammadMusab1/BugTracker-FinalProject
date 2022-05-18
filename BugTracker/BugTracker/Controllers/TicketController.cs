8f4400285b5a2e2b7f70c1ba0aca2e4680c6a488
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-05-18 00:46:57 -0500

    add UpdateTicket() get method, post method and view.

--- BugTracker/BugTracker/Controllers/TicketController.cs
+++ BugTracker/BugTracker/Controllers/TicketController.cs
@@ -9,16 +9,20 @@ namespace BugTracker.Controllers
 {
     public class TicketController : Controller
     {
+        private ApplicationDbContext _db { get; set; }
         private ProjectRepository _projectRepo { get; set; }
         private TicketRepository _ticketRepo { get; set; }
+        private TicketHistoryRepository _ticketHistoryRepo { get; set; }
         private UserManager<ApplicationUser> _userManager { get; set; }
         private RoleManager<IdentityRole> _roleManager { get; set; }
         public TicketController(ApplicationDbContext Db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
+            _db = Db;
             _userManager = userManager;
             _roleManager = roleManager;
             _projectRepo = new ProjectRepository(Db);
             _ticketRepo = new TicketRepository(Db);
+            _ticketHistoryRepo = new TicketHistoryRepository(Db);
         }
 
         public IActionResult Index()
@@ -61,6 +65,117 @@ namespace BugTracker.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        //https://localhost:7045/ticket/updateTicket?ticketId=5
+        [HttpGet]
+        public IActionResult UpdateTicket(int? ticketId)
+        {
+            if(ticketId != null)
+            {
+                try
+                {
+                    Ticket ticket = _ticketRepo.Get(ticket => ticket.Id == ticketId);
+                    return View(ticket);
+                }
+                catch (Exception ex)
+                {
+                    return NotFound("Ticket cannot be found at UpdateTicket get method");
+                }
+            }
+            else
+            {
+                return BadRequest("ticketId is null at UpdateTicket get method");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTicket(int? ticketId, Ticket updatedTicket)
+        {
+            if(ticketId != null)
+            {
+                try
+                {
+                    ApplicationUser userUpdatingTheTicket = await _userManager.FindByEmailAsync(User.Identity.Name);
+                    Ticket ticket = _ticketRepo.Get(ticket => ticket.Id == ticketId);
+                    TicketHistory ticketHistory = new TicketHistory();
+                    TicketLogItem ticketLogItem = new TicketLogItem();//mirage of the old ticket
+                    //Title, Description, Type, status, Priority are the only properties that can be change
+                    //Assigning and Unassigning developer to ticket will be handled in a different method
+                    if(ticket.Title != updatedTicket.Title)
+                    {
+                        ticketHistory.PropertiesChanged.Add($"Title({updatedTicket.Title})"); //oldValue(newValue)
+                    }
+                    if(ticket.Description != updatedTicket.Description)
+                    {
+                        ticketHistory.PropertiesChanged.Add($"Description({updatedTicket.Description})");
+                    }
+                    if (ticket.Status != updatedTicket.Status)
+                    {
+                        ticketHistory.PropertiesChanged.Add($"Status({updatedTicket.Status})");
+                    }
+                    if (ticket.Priority != updatedTicket.Priority)
+                    {
+                        ticketHistory.PropertiesChanged.Add($"Priority({updatedTicket.Priority})");
+                    }
+                    if (ticket.Type != updatedTicket.Type)
+                    {
+                        ticketHistory.PropertiesChanged.Add($"Type({updatedTicket.Type})");
+                    }
+                    if(!ticketHistory.PropertiesChanged.Any()) //no properties changed
+                    {
+                        //don't save ticketHistory, ticketLogItem and update the ticket
+                        return View("Index");
+                    }
+                    else
+                    {
+
+                        //make mirage of the oldTicket
+                        ticketLogItem.OldTitle = ticket.Title;
+                        ticketLogItem.OldDescription = ticket.Description;
+                        ticketLogItem.OldStatus = ticket.Status.ToString();
+                        ticketLogItem.OldPriority = ticket.Priority.ToString();
+                        ticketLogItem.OldType = ticket.Type.ToString();
+
+                        //ticket is now updatedTicket
+                        //ticket = updatedTicket; //will a new ticket when saving
+                        ticket.Title = updatedTicket.Title;
+                        ticket.Description = updatedTicket.Description;
+                        ticket.Status = updatedTicket.Status;
+                        ticket.Priority = updatedTicket.Priority;
+                        ticket.Type = updatedTicket.Type;
+
+                        List<TicketHistory> ticketHistoriesOfTicket = _ticketHistoryRepo.GetList(ticketH => ticketH.TicketId == ticketId).ToList();
+                        List<TicketHistory> ticketHistoriesOfUser = _ticketHistoryRepo.GetList(ticketH => ticketH.UserId == userUpdatingTheTicket.Id).ToList();
+                        ticket.TicketHistories = ticketHistoriesOfTicket;
+                        userUpdatingTheTicket.TicketHistories = ticketHistoriesOfUser;
+
+                        //One to One
+                        ticketHistory.TicketLogItem = ticketLogItem;
+                        ticketHistory.TicketLogItemId = ticketLogItem.Id;
+                        //Many to Many
+                        ticket.TicketHistories.Add(ticketHistory);
+                        userUpdatingTheTicket.TicketHistories.Add(ticketHistory);
+                        ticketHistory.TicketId = ticket.Id;
+                        ticketHistory.Ticket = ticket;
+                        ticketHistory.User = userUpdatingTheTicket;
+                        ticketHistory.UserId = userUpdatingTheTicket.Id;
+                        ticketHistory.ChangedDate = DateTime.Now;
+                        ticket.UpdatedDate = DateTime.Now;
+                        //save to database
+                        _ticketHistoryRepo.Add(ticketHistory);
+                        _db.TicketLogItem.Add(ticketLogItem); //_ticketLogItem.Add(ticketLogItem)
+                        await _userManager.UpdateAsync(userUpdatingTheTicket);//acts as db.SaveChanges()
+                    }
+                    return View();
+                }
+                catch(Exception ex)
+                {
+                    return NotFound("Ticket not found at UpdateTicket post method");
+                }
+            }
+            else
+            {
+                return BadRequest("ticketId is null at UpdateTicket post method");
+            }
         }
     }
 }
