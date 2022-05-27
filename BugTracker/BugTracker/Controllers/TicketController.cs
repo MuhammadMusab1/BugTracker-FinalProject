2c03befb1bae45b4dbabe616d1e13a760dd5e3bd
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-05-26 19:47:35 -0500

    add more selective navigation among roles

--- BugTracker/BugTracker/Controllers/TicketController.cs
+++ BugTracker/BugTracker/Controllers/TicketController.cs
@@ -189,6 +189,23 @@ namespace BugTracker.Controllers
                 }
                 ViewBag.developerList = new SelectList(ticket.Project.Developers, "Id", "UserName");
                 ViewBag.ticketId = ticketId;
+                ApplicationUser currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+                if (await _userManager.IsInRoleAsync(currentUser, "Admin"))
+                {
+                    ViewBag.currentUserIsAdmin = true;
+                }
+                else
+                {
+                    ViewBag.currentUserIsAdmin = false;
+                }
+                if (await _userManager.IsInRoleAsync(currentUser, "Project Manager"))
+                {
+                    ViewBag.currentUserIsProjectManager = true;
+                }
+                else
+                {
+                    ViewBag.currentUserIsProjectManager = false;
+                }
                 return View(ticket);
             }
             else
